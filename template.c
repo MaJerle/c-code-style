06c4dd7c5ebc56ea085220d122c5c11f66aba334
tilz0R <tilen.majerle@gmail.com>
2018-08-14 23:57:13 +0200

    Added template files

---
+++ template.c
@@ -0,0 +1,44 @@
+/**
+ * \file            template.c
+ * \brief           Template source file
+ */
+
+/*
+ * Copyright (c) 2018 Tilen Majerle
+ *  
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * This file is part of my_library.
+ *
+ * Author:          Tilen MAJERLE <tilen@majerle.eu>
+ */
+#include "template.h"
+
+/**
+ * \brief			Sum `2` numbers
+ * \param[in]		a: First value
+ * \param[in]		b: Second value
+ * \return			Sum of input values
+ */
+int
+sum(int a, int b) {
+	return a + b;
+}
